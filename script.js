dece5f10fa119f10cd7154288de26ea741e9c12a
felipefuruyama2007 <136332582+felipefuruyama2007@users.noreply.github.com>
2024-10-15 10:46:12 -0300

    15 outubro

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let randomnumber =
+let randomNumber =
 Math.floor(Math.random()*10)+1;
 let attempts = 0;
-function checkguess
+function checkguess (){
+    const userguess=parseint(document.getElementById (guess).value);attempts++;
+    if (userGuess === randomNumber) {
+        document.getElementById (message).innerText = 'parabens voce acertou';
+
